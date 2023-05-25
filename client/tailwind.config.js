3c44aef2478dc39a3e001bd6d3aea3f573bd0bf7
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-25 21:44:07 +0530

    added general styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         screens: {
             xxl: { max: "1450px" },
