5a3bb24b5860b06c8d3c87fca85aebd079f4deb6
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-26 13:53:59 +0530

    images fetching api updated

--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -11,20 +11,32 @@ const upload = require("../middlewares/uploadImage");
 //upload image api
 router.post("/upload", upload.single("gallery_image"), async (req, res) => {
     if (!req.file) return res.status(400).json({ message: "Image is required" });
-    const gallery_image = req.file;
-    imageNames.push(gallery_image.filename);
+    // const gallery_image = req.file;
+    // imageNames.push(gallery_image.filename);
 
-    res.status(201).json({ message: "Image uploaded successfully", imageNameList: imageNames });
+    res.status(201).json({ message: "Image uploaded successfully" });
 });
 
 //get images api
 router.get("/", (req, res) => {
     const folderPath = path.join(__dirname, "..", "public", "images");
 
+    console.log(folderPath);
+
+    const baseURL = `${req.protocol}://${req.get("host")}/images/`;
+
     fs.readdir(folderPath, (err, files) => {
         if (err) return res.status(400).json({ message: err.message });
 
-        res.status(200).json({ images: files });
+        if (!files.length)
+            return res.status(200).json({
+                message: "Image folder is empty, please upload some images",
+                images: files,
+            });
+        files.forEach((file) => {
+            const imageFullPath = baseURL + file;
+            res.status(200).json({ images: imageFullPath });
+        });
     });
 });
 
