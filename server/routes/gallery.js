9d9e82166ec5e3b17cb25a007d9b0c063c18392b
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-27 14:58:25 +0530

    modified api

--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -1,48 +1,24 @@
 //modules
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
-
-const router = express.Router();
 
 //middleware
 const upload = require("../middlewares/uploadImage");
 
-const folderPath = path.join(__dirname, "..", "public", "images");
+//function
+const { fetchImages } = require("../utils");
+
+const router = express.Router();
 
 //upload image api
 router.post("/upload", upload.single("gallery_image"), async (req, res) => {
     if (!req.file) return res.status(400).json({ message: "Image is required" });
 
-    fs.readdir(folderPath, (err, files) => {
-        if (err) return res.status(400).json({ message: err.message });
-
-        const baseURL = `${req.protocol}://${req.get("host")}/images/`;
-        const imageFullPath = files?.map((file) => {
-            return baseURL + file;
-        });
-
-        res.status(200).json({ message: "image uploaded successfully", images: imageFullPath });
-    });
+    fetchImages(req, res);
 });
 
 //get images api
 router.get("/", (req, res) => {
-    fs.readdir(folderPath, (err, files) => {
-        if (err) return res.status(400).json({ message: err.message });
-
-        if (!files.length)
-            return res.status(200).json({
-                message: "Image folder is empty, please upload some images",
-                images: files,
-            });
-
-        const baseURL = `${req.protocol}://${req.get("host")}/images/`;
-        const imageFullPath = files?.map((file) => {
-            return baseURL + file;
-        });
-        res.status(200).json({ images: imageFullPath });
-    });
+    fetchImages(req, res);
 });
 
 module.exports = router;
